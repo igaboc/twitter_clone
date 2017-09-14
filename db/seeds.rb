9a120c631df60ec1992b666c64defd7568d44131
igaboc <isabellegaboc@hotmail.com>
2017-09-14 15:25:55 +1000

    add devise gem, model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+tweets = [
+    {message: 'I like cake'},
+    {message: 'I like taking photos of my cake'},
+    {message: 'But I would prefer to eat my cake'}
+]
+
+Tweet.create(tweets)
